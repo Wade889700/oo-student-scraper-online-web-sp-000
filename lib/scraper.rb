f8509a0b7e8a6d45152f84417e17d735db066f47
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-09 01:17:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,12 +16,28 @@ class Scraper
       }
       students << student_info
     end
-    
     students
   end
 
-  def self.scrape_profile_page(profile_url)
+ def self.scrape_profile_page(profile_url)
+   doc = Nokogiri::HTML(open(profile_url))
+   student = {}
 
-  end
+   content = doc.css(".social-icon-container a").collect{|project| project.attribute("href").value}
+     content.each do |link|
+       if link.include?("twitter")
+         student[:twitter] = link
+       elsif link.include?("linkedin")
+         student[:linkedin] = link
+       elsif link.include?("github")
+         student[:github] = link
+       elsif link.include?(".com")
+         student[:blog] = link
+       end
+     end
+   student[:profile_quote] = doc.css(".profile-quote").text
+   student[:bio] = doc.css("div.description-holder p").text
+   student
+ end
 
 end
